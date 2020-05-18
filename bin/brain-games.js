8fdffb65812583172f6c98abf7b6e02c9443f510
tvorojjog <ant0n-94@mail.ru>
2020-05-18 20:19:31 +0500

    step three

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
 
+import getUserName from '../src/cli';
+
 console.log('Welcome to the Brain Games!');
+
+console.log(`Hello, ${getUserName} !`);
---
+++ src/cli.js
@@ -0,0 +1,5 @@
+import readlineSync from 'readline-sync';
+
+export const getUserName = () => {
+    return readlineSync.question('May I have your name? ');
+};
